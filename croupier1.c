896deb369dc3c796d9549814788bf4e88a9464c0
Ruzafan <marcramiro@gmail.com>
2024-12-02 22:30:26 +0100

    crouper1 improvement

--- croupier1.c
+++ croupier1.c
@@ -8,55 +8,63 @@ char *color_blue = "\033[01;34m";
 char *color_end = "\033[00m";
 
 void
-error(char *m) {
-    write(2, m, strlen(m));
-    write(2, "\n", 1);
-    exit(0);
+error (char *m)
+{
+  write (2, m, strlen (m));
+  write (2, "\n", 1);
+  exit (0);
 }
 
 int
-main(int arc, char *arv[]) {
-    int i, n, st, pid;
-    char s[100];
-    char *args[] = {"player1", NULL};
+main (int arc, char *arv[])
+{
+  int i, n, st, pid;
+  char s[100];
+  char *args[] = { "player1", NULL };
 
-    if (arc != 2)
-        error("Error: wrong arguments.");
+  if (arc != 2)
+    error ("Error: wrong arguments.");
 
-    n = atoi(arv[1]);
+  n = atoi (arv[1]);
 
-    sprintf(s, "\n**********Start of game (%d players, pid croupier=%d)***********\n", n, getpid());
-    if (write(1, s, strlen(s)) < 0)
-        error("write");
+  sprintf (s,
+           "\n**********Start of game (%d players, pid croupier=%d)***********\n",
+           n, getpid ());
+  if (write (1, s, strlen (s)) < 0)
+    error ("write");
 
-    for (i = 0; i < n; i++) {
-        pid = fork();
-        if (pid == -1) {
-            error("fork");
-        } else if (pid == 0) {
-            execv(args[0], args);
-            error("exec");
-        } else {
-            sprintf(s, "%s[%d] pid=%d created%s\n", color_blue, getpid(), pid, color_end);
-            if (write(1, s, strlen(s)) < 0)
-                error("write");
+  // Create all players
+  for (i = 0; i < n; i++)
+    {
+      switch (pid = fork ())
+        {
+        case -1:
+          error ("fork");
+
+        case 0:
+          execv (args[0], args);
+          error ("exec");
+
+        default:
+          sprintf (s, "%s[%d] pid=%d created%s\n", color_blue, getpid (), pid,
+                   color_end);
+          if (write (1, s, strlen (s)) < 0)
+            error ("write");
         }
     }
 
-    // Wait for all children to finish
-    for (i = 0; i < n; i++) {
-        pid = wait(&st);
-        if (pid == -1)
-            error("wait");
-
-        sprintf(s, "%s[%d] pid=%d ended%s\n", color_blue, getpid(), pid, color_end);
-        if (write(1, s, strlen(s)) < 0)
-            error("write");
+  // Wait for all players to finish
+  while ((pid = wait (&st)) > 0)
+    {
+      sprintf (s, "%s[%d] pid=%d ended%s\n", color_blue, getpid (), pid,
+               color_end);
+      if (write (1, s, strlen (s)) < 0)
+        error ("write");
     }
 
-    sprintf(s, "\n**********End of game: all players have ended***********\n");
-    if (write(1, s, strlen(s)) < 0)
-        error("write");
+  sprintf (s, "\n**********End of game: all players have ended***********\n");
+  if (write (1, s, strlen (s)) < 0)
+    error ("write");
 
-    exit(0);
+  exit (0);
 }
