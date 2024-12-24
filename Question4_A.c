62cc70123efa3ac6c417f8335f35c462aff5b013
Ruzafan <marcramiro@gmail.com>
2024-12-24 17:27:44 +0100

    add question 4

---
+++ Question4_A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main() {
+    int fd[2], pid;
+    char c;
+
+    pipe(fd);
+    pid = fork();
+
+    if (pid == 0) {
+        
+        write(fd[1], "*", 1);
+    } else {
+
+        read(fd[0], &c, 1);
+        write(1, &c, 1);
+        write(1, "\n", 1);
+    }
+
+}
